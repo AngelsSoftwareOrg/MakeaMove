3811d31cd525acbb23c6b1cf65c1aa6941dd5998
angelsburger90 <71116188+angelsburger90@users.noreply.github.com>
2020-10-20 19:09:25 +0800

    Make improvement on the parameter interface to separate the read only interface to write only interface so that the integrity of the value on the source class remains untouched...

--- src/org/angelssoftware/classes/MousePointerMover.java
+++ src/org/angelssoftware/classes/MousePointerMover.java
@@ -7,6 +7,7 @@ import java.awt.Robot;
 
 import org.angelssoftware.interfaces.RobotActionInterface;
 import org.angelssoftware.interfaces.RobotMouseInterface;
+import org.angelssoftware.interfaces.readonly.RobotMouseInterfaceReadOnly;
 import org.angelssoftware.models.RobotProperties;
 import org.angelssoftware.structures.ScreenSize;
 
@@ -19,7 +20,7 @@ public class MousePointerMover extends RobotProperties implements RobotActionInt
 		commonInitialization();
 	}
 	
-	public MousePointerMover(RobotMouseInterface robotProperties) throws AWTException {
+	public MousePointerMover(RobotMouseInterfaceReadOnly robotProperties) throws AWTException {
 		commonInitialization();
 		this.setMouseHopInPx(robotProperties.getMouseHopInPx());
 		this.setMoveDirection(robotProperties.getMoveDirection());
---
+++ src/org/angelssoftware/interfaces/writeonly/RobotMouseInterfaceWriteOnly.java
@@ -0,0 +1,11 @@
+package org.angelssoftware.interfaces.writeonly;
+
+import org.angelssoftware.structures.MoveDirection;
+
+public interface RobotMouseInterfaceWriteOnly {
+
+	public void setMouseHopInPx(int mouseHopInPx);
+	
+	public void setMoveDirection(MoveDirection moveDirection);
+	
+}
