0ddf96257d32283f0c3528bd976e453245a6face
angelsburger90 <71116188+angelsburger90@users.noreply.github.com>
2020-10-20 18:22:49 +0800

    If the Maven run is on Debug mode, the java.home is missing jre, thus remove on this file as well...

--- resources/build/run_make_a_move.bat
+++ resources/build/run_make_a_move.bat
@@ -1,4 +1,4 @@
 @echo ON
-"${java.home}\\..\\bin\java" -jar ${project.build.finalName}-spring-boot.jar -mouse-direction UP -execution-period 20000 -mouse-hop-px 1
+"${java.home}\\bin\java" -jar ${project.build.finalName}-spring-boot.jar -mouse-direction UP -execution-period 20000 -mouse-hop-px 1
 
 @pause
